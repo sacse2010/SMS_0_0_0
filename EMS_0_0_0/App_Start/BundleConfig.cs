2af665219bfff4a045e00ed3217ca36a17f1bf47
sacse2010 <sacse2010@gmail.com>
2016-07-28 01:36:56 +0600

    Added boostrap.js

--- EMS_0_0_0/App_Start/BundleConfig.cs
+++ EMS_0_0_0/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace EMS_0_0_0
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
