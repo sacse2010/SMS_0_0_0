27ea56cd49c2fa2a578410d38d7ae2476c74956c
sacse2010 <sacse2010@gmail.com>
2016-08-03 23:41:39 +0600

    Profile File Upload Done

--- EMS_0_0_0/Controllers/ProfileController.cs
+++ EMS_0_0_0/Controllers/ProfileController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,6 +14,28 @@ namespace EMS_0_0_0.Controllers
         public ActionResult Profile()
         {
             return View();
+        }
+        public ActionResult UploadImage()
+        {
+            foreach (string item in Request.Files)
+            {
+                HttpPostedFileBase file = Request.Files[item] as HttpPostedFileBase;
+                string fileName = file.FileName;
+                string UploadPath = "~/Upload/";
+
+                if (file.ContentLength == 0)
+                    continue;
+                if (file.ContentLength > 0)
+                {
+                    string path = Path.Combine(HttpContext.Request.MapPath(UploadPath), fileName);
+                    string extension = Path.GetExtension(file.FileName);
+
+                    file.SaveAs(path);
+                }
+            }
+
+            return Json("Uploaded Successfully");
+
         }
     }
 }
